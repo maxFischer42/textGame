3fda1cd3c865a5d80b768188672700141b261141
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2018-10-18 11:42:53 -0700

    Imported project

---
+++ Assets/Scripts/InteractableItems.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItems : MonoBehaviour
+{
+    public List<InteractableObject> usableItemList;
+
+    public Dictionary<string, string> examineDictionary = new Dictionary<string, string>();
+    public Dictionary<string, string> takeDictionary = new Dictionary<string, string>();
+
+    [HideInInspector] public List<string> nounsInRoom = new List<string>();
+
+    Dictionary<string, ActionResponse> useDictionary = new Dictionary<string, ActionResponse>();
+    [HideInInspector] public List<string> nounsInInventory = new List<string>();
+    GameController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<GameController>();
+    }
+
+    public string GetObjectsNotInInventory(Room currentRoom, int i)
+    {
+        InteractableObject interactableInRoom = currentRoom.interactableObjectsInRoom[i];
+
+        if (!nounsInInventory.Contains(interactableInRoom.noun))
+        {
+            nounsInRoom.Add(interactableInRoom.noun);
+            return interactableInRoom.description;
+        }
+
+        return null;
+    }
+
+    public void AddActionResponsesToUseDictionary()
+    {
+        for (int i = 0; i < nounsInInventory.Count; i++)
+        {
+            string noun = nounsInInventory[i];
+
+            InteractableObject interactableObjectInInventory = GetInteractableObjectFromUsableList(noun);
+            if (interactableObjectInInventory == null)
+                continue;
+
+            for (int j = 0; j < interactableObjectInInventory.interactions.Length; j++)
+            {
+                Interaction interaction = interactableObjectInInventory.interactions[j];
+
+                if (interaction.actionResponse == null)
+                    continue;
+
+                if (!useDictionary.ContainsKey(noun))
+                {
+                    useDictionary.Add(noun, interaction.actionResponse);
+                }
+            }
+
+        }
+    }
+
+    InteractableObject GetInteractableObjectFromUsableList(string noun)
+    {
+        for (int i = 0; i < usableItemList.Count; i++)
+        {
+            {
+                return usableItemList[i];
+            }
+        }
+        return null;
+    }
+
+    public void DisplayInventory()
+    {
+        controller.LogStringWithReturn("You look in your backpack, inside you have: ");
+
+        for (int i = 0; i < nounsInInventory.Count; i++)
+        {
+            controller.LogStringWithReturn(nounsInInventory[i]);
+        }
+    }
+
+    public void ClearCollections()
+    {
+        examineDictionary.Clear();
+        takeDictionary.Clear();
+        nounsInRoom.Clear();
+    }
+
+    public Dictionary<string, string> Take(string[] separatedInputWords)
+    {
+        string noun = separatedInputWords[1];
+
+        if (nounsInRoom.Contains(noun))
+        {
+            nounsInInventory.Add(noun);
+            AddActionResponsesToUseDictionary();
+            nounsInRoom.Remove(noun);
+            return takeDictionary;
+        }
+        else
+        {
+            controller.LogStringWithReturn("There is no " + noun + " here to take.");
+            return null;
+        }
+    }
+
+    public Dictionary<string, string> Eat(string[] separatedInputWords)
+    {
+        string noun = separatedInputWords[1];
+
+        if (nounsInRoom.Contains(noun))
+        {
+            controller.LogStringWithReturn("You try to eat the " + noun + " but you can't swallow it.");
+            return null;
+        }
+        else
+        {
+            controller.LogStringWithReturn("There is no " + noun + " here to eat.");
+            return null;
+        }
+    }
+
+    public Dictionary<string, string> Lick(string[] separatedInputWords)
+    {
+        string noun = separatedInputWords[1];
+
+        if (nounsInRoom.Contains(noun))
+        {
+            controller.LogStringWithReturn("You lick the " + noun + ". It says please don't lick me.");
+            return null;
+        }
+        else
+        {
+            int a = Random.Range(0, 6);
+            switch (a)
+            {
+                case 0:
+                    controller.LogStringWithReturn("You lick the " + noun + ". It has a very odd taste.");
+                    break;
+                case 1:
+                    controller.LogStringWithReturn("You lick the " + noun + ". It tastes like a non-poisonous apple.");
+                    break;
+                case 2:
+                    controller.LogStringWithReturn("You lick the " + noun + ". It tastes like multiple blades of grass.");
+                    break;
+                case 3:
+                    controller.LogStringWithReturn("You lick the " + noun + ". Why are you licking the " + noun + "?");
+                    break;
+                case 4:
+                    controller.LogStringWithReturn("You lick the " + noun + ". Simply delicious.");
+                    break;
+                case 5:
+                    controller.LogStringWithReturn("You lick the " + noun + ". It tastes of french fries.");
+                    break;
+            }
+
+            return null;
+        }
+    }
+
+    public void UseItem(string[] separatedInputWords)
+    {
+        string nounToUse = separatedInputWords[1];
+
+        if (nounsInInventory.Contains(nounToUse))
+        {
+            if (useDictionary.ContainsKey(nounToUse))
+            {
+                bool actionResult = useDictionary[nounToUse].DoActionResponse(controller);
+                if (!actionResult)
+                {
+                    controller.LogStringWithReturn("Hmm. Nothing happens.");
+                }
+            }
+            else
+            {
+                controller.LogStringWithReturn("You can't use the " + nounToUse);
+            }
+        }
+        else
+        {
+            controller.LogStringWithReturn("There is no " + nounToUse + " in your inventory to use");
+        }
+    }
+
+}
---
+++ Assets/Scripts/Lick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TextAdventure/InputActions/Lick")]
+public class Lick : InputAction
+{
+
+    public override void RespondToInput(GameController controller, string[] separatedInputWords)
+    {
+        Dictionary<string, string> takeDictionary = controller.interactableItems.Lick(separatedInputWords);
+
+        if (takeDictionary != null)
+        {
+            controller.LogStringWithReturn(controller.TestVerbDictionaryWithNoun(takeDictionary, "lick", separatedInputWords[1]));
+        }
+    }
+
+}
